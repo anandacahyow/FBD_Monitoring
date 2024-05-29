de8ac2251b9c9bfa5d5eaeff9bc3ecff3c180332
anandacahyow <110804041+anandacahyow@users.noreply.github.com>
2024-05-29 13:04:53 +0700

    updated

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 from datetime import datetime, timedelta
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.cluster import AgglomerativeClustering
 import numpy as np
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 import warnings
 
 warnings.filterwarnings("ignore")
 
-def load_data(file_path, columns):
-    data = pd.read_csv(file_path, usecols=columns)
+def load_data(file):
+    data = pd.read_csv(file)
     data['Datetime'] = pd.to_datetime(data['Date On Shift'] + ' ' + data['Time'])
     
     for i in range(1, len(data)):
@@ -37,11 +39,10 @@ def preprocess_data(data):
 
 def create_correlation_heatmap(data):
     correlation_matrix = data.corr()
-    correlation_matrix.to_excel("correlation_matrix.xlsx")
     fig_corr_heatmap = px.imshow(correlation_matrix,
                                  labels=dict(x="Numerical Columns", y="Numerical Columns"))
     fig_corr_heatmap.update_layout(title="Correlation Heatmap")
-    fig_corr_heatmap.write_html("correlation_heatmap.html")
+    st.plotly_chart(fig_corr_heatmap)
 
 def apply_pca(data, n_components):
     scaler = StandardScaler()
@@ -50,94 +51,40 @@ def apply_pca(data, n_components):
     pca = PCA(n_components=n_components)
     pca_result = pca.fit_transform(scaled_data)
     
-    principal_components = pca.components_
-    pc_df = pd.DataFrame(principal_components, columns=data.columns)
-    pc_df.to_excel("Database/PCA_components.xlsx")
+    return pca_result
+
+def plot_clusters(algorithm, data, pca_result, numeric_cols, data_index):
+    cluster_labels = algorithm.labels_
+    data.insert(0, 'Cluster', cluster_labels)
     
-    eigenvalues = pca.explained_variance_
-    percentage = (eigenvalues / eigenvalues.sum()) * 100
-    eigenvalues_df = pd.DataFrame({'Eigenvalues (Explained Variance)': eigenvalues, 'Percentage': percentage})
-    eigenvalues_df.to_excel("Database/eigen.xlsx")
+    combined_data = pd.concat([data_index, data], axis=1)
     
-    return pca_result, principal_components
-
-def plot_clusters(algorithms, data, pca_result, numeric_cols, data_index):
-    method_names = [type(algorithm).__name__ for algorithm in algorithms]
-    fig = make_subplots(rows=len(algorithms), cols=1, subplot_titles=method_names)
-    fig_timeseries_cluster = make_subplots(rows=len(numeric_cols), cols=1, subplot_titles=numeric_cols, shared_xaxes=True, vertical_spacing=0.01, horizontal_spacing=0.1)
-
-    for i, algorithm in enumerate(algorithms, start=1):
-        algorithm.fit(pca_result)
-        
-        cluster_labels = algorithm.labels_
-        data.insert(0, f'Cluster_{type(algorithm).__name__}', cluster_labels)
-    
-        combined_data = pd.concat([data_index, data], axis=1)
-        combined_data.to_excel(f'Database/{type(algorithm).__name__}_clustered_data.xlsx', index=False)
-        
-        for cluster_label in np.unique(cluster_labels):
-            cluster_data = pca_result[cluster_labels == cluster_label]
-            sorted_clusters = sorted(
-                np.unique(cluster_labels), 
-                key=lambda cluster_label: np.mean(pca_result[cluster_labels == cluster_label], axis=0)[0]
-            )
-            text = data_index['Recipe Name']
-            text2 = data_index['Datetime'].astype(str)
-            fig.add_trace(go.Scatter(
-                x=cluster_data[:, 0], 
-                y=cluster_data[:, 1], 
-                mode='markers', 
-                marker=dict(color=cluster_label, colorscale='viridis', size=8), 
-                name=f'Cluster_{cluster_label}',
-                legendgroup=f'Cluster_{cluster_label}',
-                hoverinfo='text',
-                text=text + "<br>" + text2,
-                showlegend=True
-            ), row=i, col=1)
-
-        fig.update_layout(title_text=type(algorithm).__name__)
-        
-        unique_clusters = data[f'Cluster_{type(algorithm).__name__}'].unique()
-        hsl_colors = ['hsl(200, 80%, 50%)', 'hsl(100, 80%, 50%)', 'hsl(0, 80%, 50%)']
-        cluster_colors = {cluster: color for cluster, color in zip(sorted_clusters, hsl_colors)}
-
-        for j, col in enumerate(numeric_cols, start=1):
-            cluster_col = data[f'Cluster_{type(algorithm).__name__}']
-            fig_timeseries_cluster.add_trace(go.Scatter(x=data_index['Datetime'], y=data[col], mode='lines', name=col, marker=dict(color='grey', size=5)), row=j, col=1)
-            fig_timeseries_cluster.add_trace(go.Scatter(x=data_index['Datetime'], y=data[col], mode='markers', name=col, marker=dict(color=[cluster_colors[c] for c in cluster_col], size=5)), row=j, col=1)
-            fig_timeseries_cluster.update_xaxes(showticklabels=True, row=j, col=1)
-    
-    fig_timeseries_cluster.update_layout(height=8000, width=1000, title=f"MaggiPCF FBD: Measurement Timeseries Plot for {type(algorithm).__name__}", showlegend=False)
-    fig_timeseries_cluster.show()
-
-    fig.update_layout(title="Clustering Visualization", showlegend=True, height=3000, width=1100)
-    fig.show()
+    st.dataframe(combined_data)
 
 def main():
     st.title('Your App Title')
-    st.sidebar.title('Sidebar Title')
-
-    # Add Streamlit widgets and organize layout here
+    st.sidebar.title('Upload CSV File')
     
-    file_path = "//idpjfl0017/MAGGI/Maggi_EXT_FBDReport.csv"
-    columns = ['Date On Shift', 'Time', 'Shift Name', 'Recipe Name', 'Operator Name', 'FLM  Name',
-               'TR6701 Temp. 1', 'TR6702 Temp. 2', 'TR6702 Temp. 3', 'TR6702 Temp. 4', 'TR6705 Ex. Temp.',
-               'PT6701 Press. 1', 'PT6702 Press. 2', 'PT6703 Press. 3', 'PT6704 Press. 4',
-               'DP6701 Diff Press. 1', 'DP6702 Diff Press. 2', 'DP6703 Diff Press. 3', 'DP6704 Diff Press. 4',
-               'TT6701 Mix. Temp. 1', 'TT6702 Mix. Temp. 2', 'TT6703 Mix. Temp. 3', 'TT6704 Mix. Temp. 4',
-               'PT6711 Mix. Press. 1', 'PT6712 Mix. Press. 2', 'M6701 Speed Exh', 'M6702 Speed Blwr 1',
-               'M6703 Speed Blwr 2', 'M6704 Speed Blwr 3', 'M6705 Speed Blwr 4', 'M6706 Speed FBD Vibr',
-               'IS_SteamSupply', 'IS_SteamFlow', 'IS_SteamFlowTOT']
+    file = st.sidebar.file_uploader("Upload CSV", type=['csv'])
     
-    data = load_data(file_path, columns)
-    data, data_index, numeric_cols = preprocess_data(data)
-    create_correlation_heatmap(data)
-    
-    n_components = len(data.columns)
-    pca_result, principal_components = apply_pca(data, n_components)
-    
-    algorithms = [AgglomerativeClustering(n_clusters=3)]
-    plot_clusters(algorithms, data, pca_result, numeric_cols, data_index)
+    if file is not None:
+        data = load_data(file)
+        st.write("Data loaded successfully!")
+        
+        if st.sidebar.button("Preprocess Data"):
+            data, data_index, numeric_cols = preprocess_data(data)
+            st.write("Data preprocessed successfully!")
+            
+            if st.sidebar.button("Create Correlation Heatmap"):
+                create_correlation_heatmap(data)
+            
+            if st.sidebar.button("Apply PCA"):
+                n_components = len(data.columns)
+                pca_result = apply_pca(data, n_components)
+                
+                if st.sidebar.button("Plot Clusters"):
+                    algorithm = AgglomerativeClustering(n_clusters=3)
+                    plot_clusters(algorithm, data, pca_result, numeric_cols, data_index)
 
 if __name__ == "__main__":
     main()
